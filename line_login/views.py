ed0928438a40e66bc91858ad022166975279a570
kyo <odinxp@gmail.com>
2021-05-01 23:56:05 +0800

    migrate資料庫並加入admin

--- line_login/views.py
+++ line_login/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+
 
 # Create your views here.
 def main(request):
     """首頁(登入頁面)
     """
-    return render(request, 'index.html')
+    content = User.objects.all()
+    return render(request, 'main.html', locals())
+
+
+def log_in(request):
+    """登入頁面(轉址至LINE)
+    """
+    pass
+
+
+def log_out(request):
+    """登出
+    """
+    logout(request)
+    return redirect('/')
